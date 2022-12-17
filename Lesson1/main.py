27bdf79dab7156c47b3883264cb08fbdaeca28d3
Dmitry <dz5011@yandex.ru>
2022-12-17 14:49:34 +0300

    basic python task2 create

--- Lesson1/main.py
+++ Lesson1/main.py
@@ -1,13 +1,15 @@
 from homework1.task1 import is_weekend
+from homework1.task2 import run_task2
 
 
 def main():
-    msg: str
-    if is_weekend(int(input("Введите день недели: "))):
-        msg = "Да"
-    else:
-        msg = "Нет"
-    print(msg)
+    # msg: str
+    # if is_weekend(int(input("Введите день недели: "))):
+    #     msg = "Да"
+    # else:
+    #     msg = "Нет"
+    # print(msg)
+    run_task2()
 
 
 if __name__ == '__main__':
