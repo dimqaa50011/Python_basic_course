b7e42dc659c81b5c545f0453126a3b3eff77bb9d
Dmitry <dz5011@yandex.ru>
2022-12-20 16:36:40 +0300

    basic python lesson2 task2

--- Lesson2/main.py
+++ Lesson2/main.py
@@ -1,9 +1,13 @@
 from hw_tasks.sum_of_digits import SumDigits
+from hw_tasks.sum_seq import SeqNumbers
 
 
 def starter():
-    sd = SumDigits()
-    sd.run()
+    # sd = SumDigits()
+    # sd.run()
+    
+    seq_nums = SeqNumbers()
+    seq_nums.run()
 
 if __name__ == "__main__":
     starter()
