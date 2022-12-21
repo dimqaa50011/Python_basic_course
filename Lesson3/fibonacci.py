cbcf14ec5c30f83f969661ebde5a19b628815a63
Dmitry <dz5011@yandex.ru>
2022-12-21 16:54:21 +0300

    basic python lesson3 task5

---
+++ Lesson3/fibonacci.py
@@ -0,0 +1,28 @@
+from typing import List
+
+# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# Пример:
+# для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] Негафибоначчи
+
+
+class Task5:
+    @classmethod
+    def run(cls):
+        print(cls.get_fibonacci(int(input("Введите длину последовательности: "))))
+
+    @classmethod
+    def get_fibonacci(cls, length: int):
+        fibonacci_seq = [1, 1]
+        nega_fibonacci_seq = [1, -1]
+
+        for i in range(2, length):
+            fibonacci_seq.append(fibonacci_seq[i - 1] + fibonacci_seq[i - 2])
+
+        for i in range(2, length):
+            nega_fibonacci_seq.append(nega_fibonacci_seq[i - 2] - nega_fibonacci_seq[i - 1])
+
+        return cls.get_result(fibonacci_seq, nega_fibonacci_seq)
+
+    @classmethod
+    def get_result(cls, fibonacci_seq: List, nega_fibonacci_seq: List):
+        return ", ".join(tuple(map(str, (list(reversed(nega_fibonacci_seq)) + [0] + list(fibonacci_seq)))))
--- Lesson3/main.py
+++ Lesson3/main.py
@@ -2,13 +2,15 @@ from sum_elements import Task1
 from mult_pair import Task2
 from fruction_difference import Task3
 from convert_to_binary import Task4
+from fibonacci import Task5
 
 
 def runner():
     # Task1.run()
     # Task2.run()
     # Task3.run()
-    Task4.run()
+    # Task4.run()
+    Task5.run()
 
 
 if __name__ == "__main__":
